66ec27e3cff08fe6cbfbcedfe33abd3bbd02d68b
Pablo Tinunin <pablotinunin@gmail.com>
2022-04-18 16:03:30 -0300

    navBar sin css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,12 +3,12 @@ import './NavBar.css';
 
 function NavBar(props) {
     return (
-        <div className='nav'>
+        <div>
             <h1 className='navBar-logo'>GamerStore</h1>
-            <ul>
-                <li a href="#">Productos</li>
-                <li a href="#">Categorias</li>
-                <li a href="#">Contactanos</li>
+            <ul className='nav'>
+                <li><a href="#productos" className='item'>Productos</a></li>
+                <li><a href="#categorias" className='item'>Categorias</a></li>
+                <li><a href="#contactanos" className='item'>Contactanos</a></li>
             </ul>
         </div>
     );
